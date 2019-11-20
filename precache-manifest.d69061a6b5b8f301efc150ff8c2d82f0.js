5d1feb6f85523bb89e5e510b930749caf6072dda
Ziki Zuck <ziki.zuck@gmail.com>
2019-11-20 22:22:22 +0200

    Updates

---
+++ precache-manifest.d69061a6b5b8f301efc150ff8c2d82f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "126428b85f12ab35cd0ca2a275814a8c",
+    "url": "/instagram-ui/index.html"
+  },
+  {
+    "revision": "ec36cbea93de58e6dfc1",
+    "url": "/instagram-ui/static/css/main.ac91e7e4.chunk.css"
+  },
+  {
+    "revision": "76c0dda5d0978b0562e6",
+    "url": "/instagram-ui/static/js/2.5197064f.chunk.js"
+  },
+  {
+    "revision": "ec36cbea93de58e6dfc1",
+    "url": "/instagram-ui/static/js/main.6a5e035d.chunk.js"
+  },
+  {
+    "revision": "fd329e52972bd349eb48",
+    "url": "/instagram-ui/static/js/runtime-main.9b17314b.js"
+  }
+]);
